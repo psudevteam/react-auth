ebbb2c3aeebfa52f8a64b2a744f7752d6df2c3cb
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-05-18 01:40:00 +0700

    fixing: register push immedieatly

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -29,12 +29,13 @@ export default function Register() {
     }
   })
 
-  const { mutate } = useRegister();
+  const { mutate, isLoading } = useRegister();
 
    const onSubmit = handleSubmit(async (data) => {
     try {
-      mutate(data)
-      push("/")
+      mutate(data, {
+        onSuccess: () => push("/")
+      })
     } catch(e) {
       console.log(e)
     }
@@ -81,6 +82,7 @@ return (
           <Button
             type="submit"
             disabled={!isValid}
+            loading={isLoading}
             className="disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-600 disabled:border w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Daftar Sekarang
